4b4df0b01422ec7ec91c591af5a644e0e92fdf5f
frto027 <602706150@qq.com>
2025-11-22 03:05:54 +0800

    Update Installers.cs

--- EyeTrackingPlug/Installers.cs
+++ EyeTrackingPlug/Installers.cs
@@ -9,13 +9,14 @@ public class AppInstaller : Installer<AppInstaller>
 {
     public override void InstallBindings()
     {
-        // There is two data sources
+        // There are two data sources.
         
         // the first is UnityEyeDataProvider, which reads eye tracking data from unity directly.
         // use this if you want to interact with game UI.
         Container.BindInterfacesAndSelfTo<UnityEyeDataProvider>().AsSingle();
         
-        // the seconed is RecordOrUnityDataProvider, which reads eye tracking data not only fron unity, but also the replay file of beatleader.
+        // the seconed is RecordOrUnityDataProvider, which reads eye tracking data not only fron unity, but also the replay file of beatleader if avaliable.
+        // use this if you want display some eye related information in game.
         Container.BindInterfacesAndSelfTo<ReplayOrUnityDataProvider>().AsSingle();
     }
 }
